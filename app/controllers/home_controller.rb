091d4bd668c4163cc26bb6acd9417a0fbcab8011
Juan Pablo Sánchez Magariños <juanpsanchezm@gmail.com>
2021-01-23 12:01:56 -0300

    rails generate controller Home index
    cambio la ruta generada automaticamente a 'bienvenida'

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+# file name in snake_case and class name in PascalCase
+# class must be a subclass of ApplicationController in order to be a controller
+class HomeController < ApplicationController
+  def index
+  end
+end
