cb3a315f1597590928b175d17cc112a1b772f476
Juan Pablo <juampi56@gmail.com>
2021-01-23 17:23:48 -0300

    migration from linux

--- db/schema.rb
+++ db/schema.rb
@@ -19,11 +19,4 @@ ActiveRecord::Schema.define(version: 2021_01_23_152745) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
