6de3e282c50104c7a98a587c7c0bc68247627a4f
Juan Pablo Sánchez Magariños <juanpsanchezm@gmail.com>
2021-01-23 23:21:23 -0300

    proteger rutas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 # file name in snake_case and class name in PascalCase
 # class must be a subclass of ApplicationController in order to be a controller
 class HomeController < ApplicationController
+  before_action :authenticate_user!
   def index
   end
 end
