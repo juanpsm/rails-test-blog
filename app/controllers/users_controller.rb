8ddbb20204415e132a4778243aa395c23c638c7d
Juan Pablo Sánchez Magariños <juanpsanchezm@gmail.com>
2021-01-25 06:17:38 -0300

    profile edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def edit
   end
   def update
-    current.user.update(user_params)
+    current_user.update(user_params)
     redirect_back(fallback_location: root_path)
   end
   def user_params
