0d8b9c99c69b22bedc84106d6b4d28e3579965c8
Juan Pablo Sánchez Magariños <juanpsanchezm@gmail.com>
2021-01-25 01:49:25 -0300

    rutas rest

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,19 @@ Rails.application.routes.draw do
 
   root to: "home#index"
 
-  get 'articles', to: "articles#index"
-  get 'articles/new', to: "articles#new", as: :new_articles
-  post 'articles', to: "articles#create"
-  get 'articles/:id', to: 'articles#show'
-  get 'articles/:id/edit', to: 'articles#edit'
-  patch 'articles/:id', to: 'articles#update', as: :article
-  delete 'articles/:id', to: 'articles#destroy'
-
-  get 'articles/user/:user_id', to: "articles#from_author"
+  # get 'articles', to: "articles#index"
+  # get 'articles/new', to: "articles#new", as: :new_articles # esto se resume como new_article_path, sin la s, cambiarlo en el html
+  # post 'articles', to: "articles#create"
+  # get 'articles/:id', to: 'articles#show'
+  # get 'articles/:id/edit', to: 'articles#edit'
+  # patch 'articles/:id', to: 'articles#update', as: :article
+  # delete 'articles/:id', to: 'articles#destroy'
+  # get 'articles/user/:user_id', to: "articles#from_author"
+  # todo esto se resume en
+  resources :articles do
+    get 'user/:user_id', to: "articles#from_author", on: :collection
+  end
+  
 
   devise_for :users
 end
